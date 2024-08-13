ffc1017d5c5534f7fdb336df9aa6766ac4a838f1
antoine <antoine.thomas654@gmail.com>
2024-08-13 18:34:14 +0200

    chapter 1

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,20 +4,57 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vector>
+
+//> framedata
+struct FrameData {
+	VkSemaphore _swapchainSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+//< framedata
 
 class VulkanEngine {
 public:
 
 	bool _isInitialized{ false };
-	int _frameNumber {0};
-	bool stop_rendering{ false };
+	int _frameNumber{ 0 };
+
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
 
-	static VulkanEngine& Get();
+	//> inst_init
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+	//< inst_init
 
-	//initializes everything in the engine
+	//> queues
+	FrameData _frames[FRAME_OVERLAP];
+
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+	//< queues
+
+	//> swap_init
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+	//< swap_init
+
+		//initializes everything in the engine
 	void init();
 
 	//shuts down the engine
@@ -28,4 +65,18 @@ public:
 
 	//run main loop
 	void run();
+
+	bool stop_rendering{ false };
+private:
+
+	void init_vulkan();
+
+	void init_swapchain();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
+
+	void init_commands();
+
+	void init_sync_structures();
 };
