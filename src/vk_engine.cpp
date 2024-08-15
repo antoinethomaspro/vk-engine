329093d6e5ed02f02712c03e3eb58fc298c869bf
antoine <antoine.thomas654@gmail.com>
2024-08-15 12:10:02 +0200

    added camera

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -64,6 +64,13 @@ void VulkanEngine::init()
 
 	init_default_data();
 
+	mainCamera.velocity = glm::vec3(0.f);
+	mainCamera.position = glm::vec3(0, 0, 5);
+
+	mainCamera.pitch = 0;
+	mainCamera.yaw = 0;
+
+
 	//everything went fine
 	_isInitialized = true;
 }
@@ -325,14 +332,20 @@ void VulkanEngine::update_scene()
 		loadedNodes["Cube"]->Draw(translation * scale, mainDrawContext);
 	}
 
-	sceneData.view = glm::translate(glm::vec3{ 0,0,-5 });
+	mainCamera.update();
+
+	glm::mat4 view = mainCamera.getViewMatrix();
+
 	// camera projection
-	sceneData.proj = glm::perspective(glm::radians(70.f), (float)_windowExtent.width / (float)_windowExtent.height, 10000.f, 0.1f);
+	glm::mat4 projection = glm::perspective(glm::radians(70.f), (float)_windowExtent.width / (float)_windowExtent.height, 10000.f, 0.1f);
 
 	// invert the Y direction on projection matrix so that we are more similar
 	// to opengl and gltf axis
-	sceneData.proj[1][1] *= -1;
-	sceneData.viewproj = sceneData.proj * sceneData.view;
+	projection[1][1] *= -1;
+
+	sceneData.view = view;
+	sceneData.proj = projection;
+	sceneData.viewproj = projection * view;
 }
 
 void VulkanEngine::draw()
